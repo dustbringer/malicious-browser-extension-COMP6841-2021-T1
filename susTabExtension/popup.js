5064ce37cb48abc830f9df6a5c71f337d7c9c3e1
victa-desktop <24532111+dustbringer@users.noreply.github.com>
2021-03-30 14:10:32 +1100

    Added toggles for each feature, for testing

---
+++ susTabExtension/popup.js
@@ -0,0 +1,28 @@
+const toggles = ["passwordWatcher", "historyWatcher"];
+const toggleElems = {};
+toggles.forEach((t) => {
+    toggleElems[t] = document.getElementById(t);
+});
+
+// Check boxes
+chrome.storage.sync.get(toggles, (res) => {
+    toggles.forEach((t) => {
+        if (!res[t]) {
+            const obj = {};
+            obj[t] = false;
+            chrome.storage.sync.set(obj);
+            toggleElems[t].checked = false;
+        } else {
+            toggleElems[t].checked = res[t];
+        }
+    });
+});
+
+// Listeners on check and uncheck
+toggles.forEach((t) => {
+    toggleElems[t].addEventListener("change", () => {
+        const obj = {};
+        obj[t] = toggleElems[t].checked;
+        chrome.storage.sync.set(obj);
+    });
+});
